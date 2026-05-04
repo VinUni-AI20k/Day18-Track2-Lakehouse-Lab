6a7b0733554122e4ab9a5f9a7a0b9d0b24360e0e
PNTLinh <halinhthao2204@gmail.com>
2026-05-04 14:08:32 +0700

    notebook lightweight

--- notebooks/02_optimize_zorder.py
+++ notebooks/02_optimize_zorder.py
@@ -2,6 +2,15 @@
 # jupyter:
 #   jupytext:
 #     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
 
 # %% [markdown]
@@ -162,3 +171,5 @@ print(
 # - [ ] File count dropped substantially after compact()
 # - [ ] Stats inspection shows ~1 file covers `user_id=4242`
 # - [ ] Screenshot the printed numbers
+
+# %%
